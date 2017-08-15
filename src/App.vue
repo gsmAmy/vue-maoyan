9891e7d58abbd1f06d8f575c91f4425db522004c
gsm <1746457008@qq.com>
2017-08-15 23:45:25 +0800

    maoyanhello

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view></router-view>
   </div>
 </template>
@@ -16,25 +15,31 @@ export default {
 	//物理分辨率
 	var clientWidth = screenWidth * pixelRatio;
 	//设置html字体大小
-	document.getElementsByTagName("html")[0].style.fontSize = clientWidth / 10 + 'px';
+	// document.getElementsByTagName("html")[0].style.fontSize = clientWidth / 10 + 'px';
+  	document.documentElement.style.fontSize = clientWidth / 10 + 'px';
 	//动态设置meta
 	var meta = document.createElement("meta");
 	meta.name = 'viewport';
 	meta.content = 'initial-scale='+1/pixelRatio+', maximum-scale='+1/pixelRatio+', minimum-scale='+1/pixelRatio+', user-scalable=no';
 	document.getElementsByTagName("head")[0].appendChild(meta);
-	
+	//设置dpr
+	var dpr = 1;
+	if(pixelRatio >=2 && pixelRatio <3){
+		dpr = 2
+	}else if(pixelRatio>=3){
+		dpr = 3;
+	}
+	document.documentElement.dataset.dpr = dpr;
   }
 }
 </script>
 
-<style>
+<style lang="less">
+@import "./assets/less/common.less";
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
-
 </style>
